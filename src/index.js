2ffeb85520eddb601b4d507683245276576e5954
jenna-lab <jennifer.sammy@student.moringaschool.com>
2022-12-16 15:36:18 +0300

    displaydetails

--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ etch("http://localhost:3000/types")
   .then((types) => renderTypes(types));
 
 const renderTypes = (types) => {
-  //   onsole.log(types);
+  // console.log(types);
+  const typesList = document.getElementById(`types-list`);
+  //displaying list in the browser
+  types.forEach((type) => {
+    const li = document.createElement("li");
+    li.innerText = type.title;
+    typesList.append(li);
+    li.addEventListener(`click`, () => renderCoffeeDetails(type));
+  });
+};
